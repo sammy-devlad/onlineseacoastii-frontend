52ac16663b2934770244231aca84fd403cbdef29
samuel aniekan <samuelaniekan680@gmail.com>
2023-04-30 17:39:49 +0100

    modified

--- assets/useAxiosPrivate-04600e2e.js
+++
@@ -1,1 +0,0 @@
-import{n as t,h as o,B as r}from"./index-3598420a.js";const u=()=>{const e=t(s=>s.user.token);return o.create({baseURL:r,headers:{Authorization:`Token ${e}`}})};export{u};
---
+++ assets/useAxiosPrivate-710fd71f.js
@@ -0,0 +1,1 @@
+import{n as t,h as o,B as r}from"./index-8789f268.js";const u=()=>{const e=t(s=>s.user.token);return o.create({baseURL:r,headers:{Authorization:`Token ${e}`}})};export{u};
